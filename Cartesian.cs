8a7bf6ac5a05c4a85716f17b29eff681fb1e8379
shadow-island <uudoin@gmail.com>
2016-10-30 22:52:59 -0600

    Add files via upload

---
+++ Cartesian.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    public static class Cartesian
+    {
+        public static IEnumerable<IEnumerable<T>> CartesianProduct<T>
+            (this IEnumerable<IEnumerable<T>> sequences)
+        {
+            IEnumerable<IEnumerable<T>> emptyProduct 
+                = new[] { Enumerable.Empty<T>() };
+            return sequences.Aggregate(
+                emptyProduct,
+                (accumulator, sequence) =>
+                    from accseq in accumulator
+                    from item in sequence
+                    select accseq.Concat(new[] { item }));
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] items = {
+                    new[] { 1, 2, 3 },
+                    new[] { 4, 5 },
+                    new[] { 6 }
+                };
+            var routes = CartesianProduct(items);
+            foreach (var route in routes)
+                Console.WriteLine(string.Join(", ", route));
+        }
+    }
+}
